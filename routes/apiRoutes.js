1c6492dbb8e637fb0d62ed0d062946fc5b6395f2
Emily Carlisle <emilydeluna96@gmail.com>
2020-05-31 22:33:59 -0500

    render newnote

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,12 +23,20 @@ module.exports = function(app) {
         fs.writeFile("db/db.json", noteString, function (err) {
           if (err) throw err;
           console.log("Note Sent!");
-
+          res.send();
+            
         });
     });
+    // req.params.id
+    app.delete("/api/notes/:id", function(req, res){
+        var deleteNote = req.params.id;
+        // delete from array
+        // note.(method to delete from array)
+        // writeFile/stringify
+        console.log(deleteNote);
+    })
 
-    
-
+    // rearrange the notes, slack
 };
 
 // npm uuid, version 1 or version 4, require uuid, import, call that function thatll create a string, as a dependency
